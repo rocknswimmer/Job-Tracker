6c4b3aae8a246f326139e6dea45eaf3a43361048
Andy Simmons <rocknswimmer116@gmail.com>
2025-06-11 14:52:27 -0400

    updated base off what I needed for notes

--- server/sql/job_tracker_test.sql
+++ server/sql/job_tracker_test.sql
@@ -29,7 +29,8 @@ begin
 
     insert into tracked (job_title, status, url, description) values
     ("junior dev", "APPLIED", "fakeurl.com", "description here"),
-    ("job to delete", "FINAL", "deleteurl.com", "need a job with no notes to delete");
+    ("job to delete", "FINAL", "deleteurl.com", "need a job with no notes to delete"),
+    ("job to update", "FINAL", "updateurl.com", "need a job to update");
     -- more here when get to tests and know
     
     insert into notes (note, job_id) values
