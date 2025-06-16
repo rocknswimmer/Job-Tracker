f27a258dd870b434a896f6c06542eb2a55057645
Andy Simmons <rocknswimmer116@gmail.com>
2025-06-16 14:16:54 -0400

    controller tested with http file

--- server/src/main/Java/job_tracker/controllers/NoteController.java
+++ server/src/main/Java/job_tracker/controllers/NoteController.java
@@ -1,6 +1,7 @@
 package job_tracker.controllers;
 
 import job_tracker.domain.NoteService;
+import job_tracker.domain.Result;
 import job_tracker.models.Note;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,5 +27,37 @@ public class NoteController {
     @GetMapping("/note/{noteId}")
     public Note findById(@PathVariable int noteId){
         return service.findById(noteId);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody Note note){
+        Result<Note> result = service.add(note);
+        System.out.println(note);
+        if(result.isSuccess()){
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
+    }
+
+    @PutMapping("/note/{noteId}")
+    public ResponseEntity<Object> update(@PathVariable int noteId, @RequestBody Note note){
+        if(noteId != note.getNoteId()){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        Result<Note> result = service.update(note);
+        if(result.isSuccess()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return ErrorResponse.build(result);
+    }
+
+    @DeleteMapping("/note/{noteId}")
+    public ResponseEntity<Void> deleteById(@PathVariable int noteId){
+        if(service.deleteById(noteId).isSuccess()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
