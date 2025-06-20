5b39055bcf5e8eec0a0f20710c6b11ec7bd2e3b5
Andy Simmons <rocknswimmer116@gmail.com>
2025-06-20 14:09:04 -0400

    mocking out routes

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import JobContainer from './jobContainer';
 
 function App() {
 
   return (
-    <div>
+    <>
+    <header>
       <h1>Job Tracker</h1>
-    </div>
+    </header>
+    <Router>
+      <Routes>
+        <Route path="/" element={<JobContainer />}></Route>
+      </Routes>
+    </Router>
+    </>
   )
 }
 
---
+++ client/src/jobContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function JobContainer() {
+
+  return(
+    <>
+    <h2>job container goes here</h2>
+    </>
+  )
+}
+
+export default JobContainer;
