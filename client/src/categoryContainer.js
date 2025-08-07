f1f24cd955b0da66afa67e715478e34c14801d50
Andy Simmons <rocknswimmer116@gmail.com>
2025-08-07 15:08:27 -0400

    more containing

--- client/src/categoryContainer.js
+++ client/src/categoryContainer.js
@@ -5,14 +5,18 @@ function CategoryContainer({category, jobs}) {
   return(
     <div class="d-flex flex-column">
     <h2>{category} jobs</h2>
-    {categoryJobs.length > 0 && categoryJobs.map(job => {
+    {categoryJobs.length > 0 &&
+    <div class="d-flex flex-column">
+      {categoryJobs.map(job => {
       return (
+        //accodion here
         <>
         <h3>job component</h3>
         <p>{job.title}</p>
         </>
       )
     })}
+    </div>}
     {categoryJobs.length === 0 && <h3>No {category} jobs</h3>}
     </ div>
   )
